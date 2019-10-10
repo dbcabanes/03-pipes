dfdae84de68d54aeb2440a5f8a7426c3978284bb
dbcabanes <dborrascabanes@hotmail.com>
2019-10-10 13:53:56 +0200

    pipes propias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-
   nombre = "Daniel";
-  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  nombre2 = "dAnIEl bOrrAs cAbAnEs";
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+  a: number = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: " primera ",
+      casa: "19"
+    }
+  };
+
+  valorDePromesa = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Llegó la data!"), 3500);
+  });
+
+  fecha = new Date();
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { LOCALE_ID } from "@angular/core";
+import localeES from "@angular/common/locales/es";
+import { registerLocaleData } from "@angular/common";
+import { CapitalizadoPipe } from "./pipes/capitalizado.pipe";
+registerLocaleData(localeES);
+
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent, CapitalizadoPipe],
+  imports: [BrowserModule],
+  providers: [{ provide: LOCALE_ID, useValue: "es" }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
